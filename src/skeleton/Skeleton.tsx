6b4544386961bc5c1b7242f131dd964b4ee4688c
YilunSun <osirisnero@gmail.com>
2022-06-13 19:46:10 +0800

    fix: component cannot be used as a jsx component error

--- src/skeleton/Skeleton.tsx
+++ src/skeleton/Skeleton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import isNumber from 'lodash/isNumber';
 import classNames from 'classnames';
 import { SkeletonRowCol, SkeletonRowColObj, TdSkeletonProps } from './type';
@@ -22,91 +22,93 @@ const ThemeMap: Record<TdSkeletonProps['theme'], SkeletonRowCol> = {
   ],
 };
 
-const Skeleton = (props: SkeletonProps) => {
-  const { animation, loading = true, rowCol, theme, className, style } = props;
+const Skeleton = forwardRef<HTMLDivElement, SkeletonProps>(
+  ({ animation, loading = true, rowCol, theme, className, style, children }, ref) => {
+    // const { animation, loading = true, rowCol, theme, className, style } = props;
 
-  const { classPrefix } = useConfig();
-  const name = `${classPrefix}-skeleton`; // t-skeleton
+    const { classPrefix } = useConfig();
+    const name = `${classPrefix}-skeleton`; // t-skeleton
 
-  const renderCols = (_cols: Number | SkeletonRowColObj | Array<SkeletonRowColObj>) => {
-    const getColItemClass = (obj: SkeletonRowColObj) =>
-      classNames(`${name}__col`, `${name}--type-${obj.type || 'text'}`, {
-        [`${name}--animation-${animation}`]: animation,
-      });
+    const renderCols = (_cols: Number | SkeletonRowColObj | Array<SkeletonRowColObj>) => {
+      const getColItemClass = (obj: SkeletonRowColObj) =>
+        classNames(`${name}__col`, `${name}--type-${obj.type || 'text'}`, {
+          [`${name}--animation-${animation}`]: animation,
+        });
 
-    const getColItemStyle = (obj: SkeletonRowColObj): Styles => {
-      const styleName = [
-        'width',
-        'height',
-        'marginRight',
-        'marginLeft',
-        'margin',
-        'size',
-        'background',
-        'backgroundColor',
-      ];
-      const style: Styles = {};
-      styleName.forEach((name) => {
-        if (name in obj) {
-          const px = pxCompat(obj[name]);
-          if (name === 'size') {
-            [style.width, style.height] = [px, px];
-          } else {
-            style[name] = px;
+      const getColItemStyle = (obj: SkeletonRowColObj): Styles => {
+        const styleName = [
+          'width',
+          'height',
+          'marginRight',
+          'marginLeft',
+          'margin',
+          'size',
+          'background',
+          'backgroundColor',
+        ];
+        const style: Styles = {};
+        styleName.forEach((name) => {
+          if (name in obj) {
+            const px = pxCompat(obj[name]);
+            if (name === 'size') {
+              [style.width, style.height] = [px, px];
+            } else {
+              style[name] = px;
+            }
           }
-        }
-      });
-      return style;
+        });
+        return style;
+      };
+
+      let cols: Array<SkeletonRowColObj> = [];
+      if (Array.isArray(_cols)) {
+        cols = _cols;
+      } else if (isNumber(_cols)) {
+        cols = new Array(_cols).fill({ type: 'text' });
+      } else {
+        cols = [_cols as SkeletonRowColObj];
+      }
+
+      return cols.map((item, index) => (
+        <div key={index} className={getColItemClass(item)} style={getColItemStyle(item)}>
+          {typeof item.content === 'function' ? item.content(React.createElement) : item.content}
+        </div>
+      ));
     };
 
-    let cols: Array<SkeletonRowColObj> = [];
-    if (Array.isArray(_cols)) {
-      cols = _cols;
-    } else if (isNumber(_cols)) {
-      cols = new Array(_cols).fill({ type: 'text' });
-    } else {
-      cols = [_cols as SkeletonRowColObj];
+    const renderRowCol = (_rowCol?: SkeletonRowCol) => {
+      const renderedRowCol: SkeletonRowCol = _rowCol || rowCol;
+
+      return renderedRowCol.map((item, index) => (
+        <div key={index} className={`${name}__row`}>
+          {renderCols(item)}
+        </div>
+      ));
+    };
+
+    if (!loading) {
+      return <div>{children}</div>;
     }
 
-    return cols.map((item, index) => (
-      <div key={index} className={getColItemClass(item)} style={getColItemStyle(item)}>
-        {typeof item.content === 'function' ? item.content(React.createElement) : item.content}
+    const childrenContent = [];
+    if (theme) {
+      childrenContent.push(renderRowCol(ThemeMap[theme]));
+    }
+    if (rowCol) {
+      childrenContent.push(renderRowCol(rowCol));
+    }
+    if (!theme && !rowCol) {
+      // 什么都不传时，传入默认 rowCol
+      childrenContent.push(renderRowCol([1, 1, 1, { width: '70%' }]));
+    }
+
+    return (
+      <div className={className} style={style} ref={ref}>
+        {childrenContent}
       </div>
-    ));
-  };
-
-  const renderRowCol = (_rowCol?: SkeletonRowCol) => {
-    const renderedRowCol: SkeletonRowCol = _rowCol || rowCol;
-
-    return renderedRowCol.map((item, index) => (
-      <div key={index} className={`${name}__row`}>
-        {renderCols(item)}
-      </div>
-    ));
-  };
-
-  if (!loading) {
-    return <div>{props.children}</div>;
-  }
-
-  const childrenContent = [];
-  if (theme) {
-    childrenContent.push(renderRowCol(ThemeMap[theme]));
-  }
-  if (rowCol) {
-    childrenContent.push(renderRowCol(rowCol));
-  }
-  if (!theme && !rowCol) {
-    // 什么都不传时，传入默认 rowCol
-    childrenContent.push(renderRowCol([1, 1, 1, { width: '70%' }]));
-  }
-
-  return (
-    <div className={className} style={style}>
-      {childrenContent}
-    </div>
-  );
-};
+    );
+  },
+);
 
 Skeleton.displayName = 'Skeleton';
 
